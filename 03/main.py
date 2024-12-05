6ff65ea12475a07e6fd873b7b742c167deac3f18
Jake Ascher <ascherj@gmail.com>
2024-12-05 16:42:44 -0700

    Complete Part 2 of Day 3

--- 03/main.py
+++ 03/main.py
@@ -20,9 +20,32 @@ class CorruptedMemoryScanner:
     def _sum_valid_mul_instructions(self) -> int:
         return sum(int(a) * int(b) for a, b in self._find_valid_mul_instructions())
 
+    def _process_data(self) -> int:
+        sum = 0
+        dont_text = "don't()"
+        do_text = "do()"
+        enabled = True
+        curr = 0
+        while curr < len(self.data):
+            if self.data[curr:].startswith(dont_text):
+                curr += len(dont_text)
+                enabled = False
+            elif self.data[curr:].startswith(do_text):
+                curr += len(do_text)
+                enabled = True
+            else:
+                pattern = r"mul\((\d{1,3}),\s*(\d{1,3})\)"
+                match = re.match(pattern, self.data[curr:])
+                if match and enabled:
+                    sum += int(match.group(1)) * int(match.group(2))
+                    curr += len(match.group())
+                else:
+                    curr += 1
+        return sum
+
 def main():
     scanner = CorruptedMemoryScanner("input.txt")
-    print(scanner._sum_valid_mul_instructions())
+    print(scanner._process_data())
 
 if __name__ == "__main__":
     main()
