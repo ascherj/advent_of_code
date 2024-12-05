8ae42203feb3c5e5cea5428ff2e4b43797a2775d
Jake Ascher <ascherj@gmail.com>
2024-12-05 11:04:21 -0700

    Classify 02/main.py

--- 02/main.py
+++ 02/main.py
@@ -4,52 +4,56 @@ Level = NewType("Level", str)
 Report = NewType("Report", list[Level])
 ReportList = NewType("ReportList", list[Report])
 
-def main():
-    def read_data() -> ReportList:
-        with open("input.txt", "r") as f:
+class ReportAnalyzer:
+    def __init__(self, filename: str):
+        self.filename = filename
+        self.data = self._read_data()
+
+    def _read_data(self) -> ReportList:
+        with open(self.filename, "r") as f:
             return [Report(report.split()) for report in f.read().splitlines()]
 
-    def is_no_more_than_three_diff(report: Report) -> bool:
+    def _is_no_more_than_three_diff(self, report: Report) -> bool:
         return all(
             abs(int(report[i + 1]) - int(report[i])) <= 3
             for i in range(len(report) - 1)
         )
 
-    def is_increasing(report: Report) -> bool:
+    def _is_increasing(self, report: Report) -> bool:
         return all(
             int(report[i]) < int(report[i + 1])
             for i in range(len(report) - 1)
         )
 
-    def is_decreasing(report: Report) -> bool:
+    def _is_decreasing(self, report: Report) -> bool:
         return all(
             int(report[i]) > int(report[i + 1])
             for i in range(len(report) - 1)
         )
 
-    def is_good_report(report: Report) -> bool:
-        return is_no_more_than_three_diff(report) and (
-            is_increasing(report) or is_decreasing(report)
+    def _is_good_report(self, report: Report) -> bool:
+        return self._is_no_more_than_three_diff(report) and (
+            self._is_increasing(report) or self._is_decreasing(report)
         )
 
-    def count_good_reports(data: ReportList) -> int:
+    def count_good_reports(self) -> int:
         good_reports: int = 0
 
-        for report in data:
-            if is_good_report(report):
+        for report in self.data:
+            if self._is_good_report(report):
                 good_reports += 1
             else:
                 for i in range(len(report)):
-                    modified_report = report[:i] + report[i + 1 :]
-                    if is_good_report(modified_report):
+                    modified_report = report[:i] + report[i + 1:]
+                    if self._is_good_report(modified_report):
                         good_reports += 1
                         break
 
         return good_reports
 
-    data = read_data()
-    print(count_good_reports(data))
-
+def main():
+    analyzer = ReportAnalyzer("input.txt")
+    print(analyzer.count_good_reports())
 
 if __name__ == "__main__":
     main()
