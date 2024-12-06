61013f9532925cd3071d171acd806b11cbb1f890
Jake Ascher <ascherj@gmail.com>
2024-12-06 16:12:14 -0700

    Complete Part 1 of Day 4

---
+++ 04/main.py
@@ -0,0 +1,72 @@
+from collections import deque
+
+class XmasWordSearcher:
+    def __init__(self, filename: str):
+        self.filename = filename
+        self.grid = self._create_grid()
+        self.DIRECTIONS = ((-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1))
+        self.ROWS = self._get_rows()
+        self.COLS = self._get_cols()
+
+    def _read_data(self) -> str:
+        with open(self.filename, "r") as f:
+            return f.read()
+
+    def _create_grid(self) -> list[list[str]]:
+        return [list(row) for row in self._read_data().split("\n")][:-1]
+
+    def _get_rows(self) -> int:
+        return len(self.grid)
+
+    def _get_cols(self) -> int:
+        return len(self.grid[0])
+
+    def _bfs(self, row: int, col: int, word: str) -> int:
+        matches = 0
+        q: deque[int, int, str, tuple[int, int]] = deque()
+        q.append((row, col, word, (0, 0)))
+
+        if len(word) == 1 and self.grid[row][col] == w[0]:
+            return 1
+
+        for (dr, dc) in self.DIRECTIONS:
+            if (
+                0 <= (row + dr) < self.ROWS and
+                0 <= (col + dc) < self.COLS and
+                self.grid[row + dr][col + dc] == word[1]
+            ):
+                q.append((row + dr, col + dc, word[1:], (dr, dc)))
+
+        while q:
+            r, c, w, (dr, dc) = q.popleft()
+
+            if len(w) == 1 and self.grid[r][c] == w[0]:
+                matches += 1
+                continue
+
+            if len(w) > 1:
+                if (
+                    0 <= (r + dr) < self.ROWS and
+                    0 <= (c + dc) < self.COLS and
+                    self.grid[r + dr][c + dc] == w[1]
+                ):
+                    q.append((r + dr, c + dc, w[1:], (dr, dc)))
+
+        return matches
+
+    def search_for_word(self, word: str) -> int:
+        matches = 0
+
+        for m in range(self.ROWS):
+            for n in range(self.COLS):
+                if self.grid[m][n] == word[0]:
+                    matches += self._bfs(m, n, word)
+
+        return matches
+
+def main():
+    searcher = XmasWordSearcher("input.txt")
+    print(searcher.search_for_word("XMAS"))
+
+if __name__ == "__main__":
+    main()
